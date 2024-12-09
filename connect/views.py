1f76b74acb1813713b6724a3f224b250394b2929
Quang Mai <maithanhnhatquang1@gmail.com>
2024-12-09 18:44:05 +0700

    Add index page

---
+++ connect/views.py
@@ -0,0 +1,13 @@
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+import os
+
+
+def index(request):
+    return render(request, "connect/index.html")
