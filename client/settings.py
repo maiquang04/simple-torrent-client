344bf292448e29bc412c239daea284df8c62f527
Quang Mai <maithanhnhatquang1@gmail.com>
2024-12-12 09:19:17 +0700

    Add corsheader

--- client/settings.py
+++ client/settings.py
@@ -33,10 +33,14 @@ CSRF_TRUSTED_ORIGINS = ["https://*.ngrok-free.app"]
 
 SESSION_SAVE_EVERY_REQUEST = True
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+
 # Application definition
 
 INSTALLED_APPS = [
     "connect",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -46,6 +50,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
